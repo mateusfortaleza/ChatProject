a066199ca7ac65294dad07f25a367ccaac6fec5a
Mateus <mateusvillanova@protonmail.com>
2025-01-31 08:47:13 -0300

    Updated the register to contain registration inputs.

--- register.php
+++ register.php
@@ -6,6 +6,18 @@
     <title>Register</title>
 </head>
 <body>
-    
+    <form action="register.php" method="post">
+        <h1>Register</h1>
+        <label>Name: 
+            <input type="text" name="chatName" id="name" required>
+        </label>
+        <label>Email: 
+            <input type="email" name="chatEmail" id="email" required>
+        </label>
+        <label>Password: 
+            <input type="password" name="chatPassword" id="password" required>
+        </label>
+        <input type="submit" value="Submit">
+    </form>
 </body>
 </html>
