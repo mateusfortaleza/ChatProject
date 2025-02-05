d6965487c36dad54e2b51b1677c1460083d6be8d
Mateus <mateusvillanova@protonmail.com>
2025-02-05 09:31:04 -0300

    Added atempt at making validation form

--- index.php
+++ index.php
@@ -1,3 +1,56 @@
+<?php
+    $loginSucessful = false;
+    $validForm = false;
+    $validationError = "";
+
+    function validateEmail($email) {
+        $conn = mysqli_connect('127.0.0.1','root','','mateus');    
+        if(!$conn) {
+            echo "Connection failed";
+            exit();
+        }
+        $EmailSql = "SELECT * FROM user WHERE email = '$email'";
+        $result = mysqli_query($conn, $EmailSql);
+        $numRows = mysqli_num_rows($result);
+        mysqli_close($conn);
+        if($numRows == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+    
+    if($_SERVER['REQUEST_METHOD'] == "POST") {
+        // Assign POST data
+        $formEmail = $_POST['chatEmail'];
+        $formPassword = $_POST['chatPassword'];
+
+        $validEmail = validateEmail($formEmail);
+
+        $validForm = $validEmail;
+        // Login user
+        if ($validForm) {
+            $link = mysqli_connect("127.0.0.1","root","","mateus");
+            if (!$link) {
+                echo "Connection unsuccessfull"; 
+                exit;
+            }
+        
+            $sql = "SELECT * FROM user WHERE Email = '$formEmail'";
+        
+            if (mysqli_query($link, $sql)) {
+                $loginSucessful = true;
+                header('Location: http://localhost/Chat-site/chat.php');
+            } else {
+                echo "Login unsuccessfull. Contact support if this problem persists";
+                echo mysqli_error($link);
+            }
+            
+            mysqli_close($link);
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
