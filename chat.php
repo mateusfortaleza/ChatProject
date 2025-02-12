24d5bfb410f55c7577f5f15896e5a71240841524
Mateus Fortaleza <mateusvillanova@protonmail.com>
2025-02-12 10:47:03 -0300

    Added inputs for the chat page

--- chat.php
+++ chat.php
@@ -4,9 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="">
 </head>
 <body>
-    
+    <main>
+        <h1>Chat</h1>
+        <div class="chatArea">
+            <label for="chatText">        
+            <textarea name="chatText" id="chatText"></textarea>
+            </label>
+            <input type="submit" value="Submit" class="TextSubmit" />
+        </div>
+    </main>
 </body>
 </html>
