c404ace75a7b510df4fdc82851acfc51ef9be01b
Mateus Fortaleza <mateusvillanova@protonmail.com>
2025-02-17 11:02:39 -0300

    Modified the cookie verifier so that it returns to the index page and added a message viewer

--- chat.php
+++ chat.php
@@ -1,9 +1,8 @@
 <?php 
-    if(isset($_COOKIE['user'])) {
-        return true;
-    } else {
+    if(!isset($_COOKIE['user'])) {
         header("Location: index.php");
-    };
+    }
+
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
         date_default_timezone_set("AMERICA/SAO_PAULO");
         $text = $_POST['chatText'];
@@ -14,31 +13,45 @@
             echo "Connection error: " . mysqli_connect_errno();
             die();
         }
-
+        
         $sql = "INSERT INTO messages (Sender, Message, Date) VALUES (" . $_COOKIE['user'] . ", '$text', '$date')";
         $result = mysqli_query($link, $sql);
-          
-
+        
+        
     }
-?>
+
+    // Query messages
+    
+    ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat</title>
-    <link rel="stylesheet" href="./styles.css">
-</head>
-<body>
-    <main>
-        <h1 class="chat-title">Chat</h1>
-        
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Chat</title>
+        <link rel="stylesheet" href="./styles.css">
+    </head>
+    <body>
+        <main>
+            <h1 class="chat-title">Chat</h1>
         <form class="chatArea" action="chat.php" method="POST">
             <label for="chatText" class="sr-only">Chat</label>
             <input type="text" name="chatText" id="chatText" required/>
             <input type="submit" value="Submit" class="TextSubmit" />
         </form>
+        <div><?php 
+            $linkSql = mysqli_connect("127.0.0.1", "root", "", "chat");
+            $sqlResult = "SELECT * FROM messages";
+            $resultMessages = mysqli_query($linkSql, $sqlResult);
+            
+            $numRows = mysqli_num_rows($resultMessages);
+
+            for ($i = 0; $i < $numRows; $i++) { 
+                $currentRow = mysqli_fetch_assoc($resultMessages);
+                echo $currentRow["Message"] . "</br>";
+            }
+        ?></div>
     </main>
 </body>
 </html>
