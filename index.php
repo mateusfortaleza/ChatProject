09937cb2c277f6d9595694e46be4739de7240d17
Mateus Fortaleza <mateusvillanova@protonmail.com>
2025-02-17 09:46:19 -0300

    Made a few corrections on the PHP and the HTML

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-    $loginSucessful = false;
     $validForm = false;
     $validationError = "";
-    
+    $loginSucessful = false;
     if($_SERVER['REQUEST_METHOD'] == "POST") {
         // Assign POST data
         $formEmail = $_POST['chatEmail'];
@@ -29,7 +28,7 @@
                     $loginSucessful = true;
                     $user = mysqli_fetch_assoc($result);
                     setcookie("user", $user['ID']);
-                    header('Location: http://localhost/ChatProject/chat.php');
+                    header('Location: chat.php');
                 }
 
             } else {
@@ -45,14 +44,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles.css">
     <title>Chat</title>
 </head>
 <body>
     <main>
-        <h1>Welcome to the Chat</h1>
+        <h1 class="indexTitle">Welcome to the Chat</h1>
         <form action="index.php" method="post">
             <?php if (!$loginSucessful && $_SERVER['REQUEST_METHOD'] == "POST") {?>
                 <p>Email or password incorrect. Try again</p>
@@ -66,7 +65,9 @@
             <input type="submit" value="Sign In" class="IndexButton"/>
         </form>
     </main>
-    <p>Don't have a account?</p>
-    <a href="register.php"><input type="button" value="Register Now" /></a>
+    <div class="registerRedirect">
+        <p>Don't have a account?</p>
+        <a href="register.php"><input type="button" value="Register Now" /></a>
+    </div>
 </body>
 </html>
