0f401f30804d4297375269f7ac64ec96095d9fd1
Mateus Fortaleza <mateusvillanova@protonmail.com>
2025-02-20 11:13:40 -0300

    Added Tailwind CSS

--- register.php
+++ register.php
@@ -55,15 +55,16 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full bg-white">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles.css" />
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     <title>Register Now</title>
 </head>
-<body>
-    <main>
+<body class="h-full bg-blue-700">
+    <main class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 lg:py-24">
+        <div class="bg-white shadow-lg rounded-lg p-6 lg:p-12">
         <?php if ($registerSucessful) { ?>
             <h2>Registration sucessful</h2>
             <p><a href="index.php">Back to Sign In</a></p>
@@ -71,22 +72,30 @@
             <?php if (!$validForm) { ?>
                 <h3><?php echo $validationError ?></h3>
             <?php } ?>
-            <form action="register.php" method="post">
-                <h1>Register</h1>
-                <label> Name: 
-                    <input type="text" name="chatName" id="name" required>
-                </label>
-                <label> Email: 
-                    <input type="email" name="chatEmail" id="email" required>
-                </label>
-                <label> Password: 
-                    <input type="password" name="chatPassword" id="password" required>
-                </label>
-                <input type="submit" value="Submit" class="RegisterButton"/>
-            </form>
-        </main>
-        <p> Already have a account?</p>
-        <a href="index.php"><input type="button" value="Return"></a>
-     <?php } ?>
+            <h1 class="text-[50px] flex flex-row justify-center items-center font-semibold">Register</h1>
+            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-md">
+                <form action="register.php" method="post" class="space-y-6">
+                    <div>
+                        <label for="name" class="block text-sm/6 font-medium text-gray-900">Name: </label> 
+                        <input type="text" name="chatName" id="name" placeholder="" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:-outline-offset-2 focus:outline-blue-900 sm:text-sm/6" required>
+                    </div>
+                    <div>
+                        <label class="block text-sm/6 font-medium text-gray-900">Email: </label>
+                        <input type="email" name="chatEmail" id="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:-outline-offset-2 focus:outline-blue-900 sm:text-sm/6" required>
+                    </div>
+                    <div>
+                        <label class="block text-sm/6 font-medium text-gray-900">Password: </label>
+                        <input type="password" name="chatPassword" id="password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:-outline-offset-2 focus:outline-blue-900 sm:text-sm/6" required>
+                    </div>
+                    <div>
+                        <input type="submit" value="Submit" class="flex w-full justify-center rounded-md bg-blue-900 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-blue-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:bg-blue-900"/>
+                    </div>
+                </form>
+            </div>
+            <p class="mt-10 text-center">Already have a account?</p>
+            <a href="index.php" class="font-semibold flex items-center justify-center text-blue-600 hover:text-blue-900">Sign In</a>
+             <?php } ?>
+        </div>
+    </main>
 </body>
 </html>
