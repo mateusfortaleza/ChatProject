0c79b26a98e663591a87f3aec24aaee21c1e19de
Mateus Fortaleza <mateusvillanova@protonmail.com>
2025-02-25 08:31:46 -0300

    Added endpoint for message insertion

--- message-input.php
+++ message-input.php
@@ -1,8 +1,21 @@
 <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST') 
     {
-        $rawPostBody = file_get_contents('php://input');
-        echo "Raw POST Body: $rawPostBody";
+        date_default_timezone_set("AMERICA/SAO_PAULO");
+        $text = $_POST['chatText'];
+        $user = $_POST['chatUser'];
+        $date = date("Y-m-d H:i:s");
+    
+        $link = mysqli_connect('127.0.0.1', 'root', '', 'chat');
+        if (!$link) {
+            echo "Connection error: " . mysqli_connect_errno();
+            die();
+        }
+    
+        $sql = "INSERT INTO messages (Sender, Message, Date) VALUES ($user, '$text', '$date')";
+        $result = mysqli_query($link, $sql);
+    
+        mysqli_close($link);
     }
     else
     {
