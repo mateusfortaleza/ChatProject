93bf05b17935f2256973dd5d3c46155b8e7990a2
Mateus Fortaleza <mateusvillanova@protonmail.com>
2025-02-25 10:14:29 -0300

    Added the reload messages function

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+function reloadMessages() {
+    $.ajax({
+        url: "messages.php"
+    }).done(function (data) {
+        document.getElementById("response-messages").innerHTML = "";
+        data.map(msg => {
+            const isSender = msg.MessageUserID == "<?php echo $_COOKIE['user']; ?>";
+            const messageClass = isSender ?  "bg-green-200 text-gray-900 self-start" : "bg-blue-500 text-white self-end" ;
+            const roundedClass = isSender ? "rounded-lg rounded-br-none" : "rounded-lg rounded-bl-none";
+            document.getElementById("response-messages").insertAdjacentHTML("beforeend", `
+                <div class="max-w-xs p-8 ${messageClass} ${roundedClass} shadow-md break-words overflow-hidden">
+                    <p class="text-lg font-semibold">${msg.MessageName}</p>
+                    <p class="text-xl break-words">${msg.MessageText}</p>
+                    <span class="text-xs block mt-1">${msg.MessageDate}</span>
+                </div>
+            `);
+        });
+        window.scrollTo(0, document.body.scrollHeight);
+    });
+}
+
+reloadMessages();
+setInterval(reloadMessages, 1000);
