d579aef87bda1aa409f29bed0c0fc10490fc0a34
Mateus Fortaleza <mateusvillanova@protonmail.com>
2025-02-26 11:09:39 -0300

    Simplified code for message verifier

--- message-check-new.php
+++ message-check-new.php
@@ -1,22 +1,12 @@
 <?php
-// Create connection
-$conn = mysqli_connect("127.0.0.1", 'root', "", "chat");
+    require 'business.php';
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-$sql = "SELECT MAX(DATE) FROM messages";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "Date: " . $row["date"] . "<br>";
+    if (message_check_new()) {
+        echo "{ \"newMessages\": true }";
+    } else {
+        echo "{ \"newMessages\": false }";
     }
-} else {
-    echo "No messages found";
-}
 
-mysqli_close($conn);
+    header('Content-Type: application/json');
+
 ?>
