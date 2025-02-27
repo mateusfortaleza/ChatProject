74f5b43854c8976d87d464d00393db700f31319d
Mateus Fortaleza <mateusvillanova@protonmail.com>
2025-02-27 08:44:25 -0300

    Simplified code

--- message-get-all.php
+++ message-get-all.php
@@ -1,11 +1,8 @@
 <?php
     header('Content-Type: application/json');
 
-    $linkSql = mysqli_connect("127.0.0.1", "root", "", "chat");
     $sqlResult = "SELECT MSG.Message AS MessageText, MSG.Date AS MessageDate, USR.Name AS MessageName, USR.ID AS MessageUserID FROM messages AS MSG JOIN user AS USR ON MSG.Sender = USR.ID ORDER BY MSG.Date ASC";
-    $resultMessages = mysqli_query($linkSql, $sqlResult);
+    chat_query($sqlResult);
+    $resultMessages = chat_query($sqlResult);
     $arrAllResults = mysqli_fetch_all($resultMessages, MYSQLI_ASSOC);
-
-    mysqli_close($linkSql);
-
     echo json_encode($arrAllResults);
