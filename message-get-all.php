3f76a2322a54e91496d7494be22400937e05c834
Mateus Fortaleza <mateusvillanova@protonmail.com>
2025-02-27 09:15:01 -0300

    Put chat_query into a variable

--- message-get-all.php
+++ message-get-all.php
@@ -2,7 +2,6 @@
     header('Content-Type: application/json');
 
     $sqlResult = "SELECT MSG.Message AS MessageText, MSG.Date AS MessageDate, USR.Name AS MessageName, USR.ID AS MessageUserID FROM messages AS MSG JOIN user AS USR ON MSG.Sender = USR.ID ORDER BY MSG.Date ASC";
-    chat_query($sqlResult);
     $resultMessages = chat_query($sqlResult);
     $arrAllResults = mysqli_fetch_all($resultMessages, MYSQLI_ASSOC);
     echo json_encode($arrAllResults);
