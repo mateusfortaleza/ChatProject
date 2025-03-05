75fb2d1dd6564294820ff84b4051a7d4d8643058
Mateus Fortaleza <mateusvillanova@protonmail.com>
2025-03-05 10:08:51 -0300

    Added a JSON object to check if message was inserted

--- message-insert.php
+++ message-insert.php
@@ -1,17 +1,18 @@
 <?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+require "db.php";
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
     {
         date_default_timezone_set("AMERICA/SAO_PAULO");
         $text = $_POST['chatText'];
         $user = $_POST['chatUser'];
         $date = date("Y-m-d H:i:s");
-    
-        $sql = "INSERT INTO messages (Sender, Message, Date) VALUES ($user, '$text', '$date')";
+
+
+        $sql = "INSERT INTO messages (Sender, Message, CreatedAt) VALUES ($user, '$text', '$date')";
         chat_query($sql);
+
+        echo "{ \"insertMessage\": \"success\" }";
+
+        header("Content-Type: application/json");
     
     }
-    else
-    {
-        echo "Hello GET";
-    }
-?>
