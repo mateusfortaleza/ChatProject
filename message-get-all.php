0de45bad113dcbcdffc9e74167e996b4ce79062c
Mateus Fortaleza <mateusvillanova@protonmail.com>
2025-03-05 10:08:07 -0300

    Added the link to the db.php file and changed the header location

--- message-get-all.php
+++ message-get-all.php
@@ -1,7 +1,9 @@
 <?php
-    header('Content-Type: application/json');
+    require "db.php";
 
-    $sqlResult = "SELECT MSG.Message AS MessageText, MSG.Date AS MessageDate, USR.Name AS MessageName, USR.ID AS MessageUserID FROM messages AS MSG JOIN user AS USR ON MSG.Sender = USR.ID ORDER BY MSG.Date ASC";
+    $sqlResult = "SELECT MSG.Message AS MessageText, MSG.CreatedAt AS MessageDate, USR.Name AS MessageName, USR.ID AS MessageUserID FROM messages AS MSG JOIN user AS USR ON MSG.Sender = USR.ID ORDER BY MSG.CreatedAt ASC";
     $resultMessages = chat_query($sqlResult);
     $arrAllResults = mysqli_fetch_all($resultMessages, MYSQLI_ASSOC);
     echo json_encode($arrAllResults);
+
+    header('Content-Type: application/json');
