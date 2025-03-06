39df0e252413edd7a80c3b6e79848aec08c26d6e
Mateus Fortaleza <mateusvillanova@protonmail.com>
2025-03-06 11:01:25 -0300

    Simplified the array code

--- db.php
+++ db.php
@@ -1,11 +1,12 @@
 <?php
+
 function mysql_escape_utf8($string) {
     // Ensure the string is in UTF-8 encoding
     $string = mb_convert_encoding($string, 'UTF-8', 'UTF-8');
 
     // Escape special characters
-    $search  = array("\\",  "\x00", "\n",  "\r",  "'",  '"', "\x1a");
-    $replace = array("\\\\", "\\0", "\\n", "\\r", "\'", '\"', "\\Z");
+    $search  = ["\\",  "\x00", "\n",  "\r",  "'",  '"', "\x1a"];
+    $replace = ["\\\\", "\\0", "\\n", "\\r", "\'", '\"', "\\Z"];
 
     return str_replace($search, $replace, $string);
 }
