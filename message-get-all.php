4e48b9293af35cb78ee1524dd479fd70a0b25f16
Mateus Fortaleza <mateusfortaleza@protonmail.com>
2025-03-13 11:48:23 -0300

    Just added a new line

--- message-get-all.php
+++ message-get-all.php
@@ -1,9 +1,9 @@
 <?php
-    require "db.php";
+require "db.php";
 
-    $sqlResult = "SELECT MSG.Message AS MessageText, MSG.CreatedAt AS MessageDate, USR.Name AS MessageName, USR.ID AS MessageUserID FROM messages AS MSG JOIN user AS USR ON MSG.Sender = USR.ID ORDER BY MSG.CreatedAt ASC";
-    $resultMessages = chat_query($sqlResult);
-    $arrAllResults = mysqli_fetch_all($resultMessages, MYSQLI_ASSOC);
-    echo json_encode($arrAllResults);
+$sqlResult = "SELECT MSG.Message AS MessageText, MSG.CreatedAt AS MessageDate, USR.Name AS MessageName, USR.ID AS MessageUserID FROM messages AS MSG JOIN user AS USR ON MSG.Sender = USR.ID ORDER BY MSG.CreatedAt ASC";
+$resultMessages = chat_query($sqlResult);
+$arrAllResults = mysqli_fetch_all($resultMessages, MYSQLI_ASSOC);
+echo json_encode($arrAllResults);
 
-    header('Content-Type: application/json');
+header('Content-Type: application/json');
