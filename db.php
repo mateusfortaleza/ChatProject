af5259e0fd45f50752e39627717be40912a71fc2
Mateus Fortaleza <mateusfortaleza@protonmail.com>
2025-03-13 11:48:08 -0300

    Minor modifications

--- db.php
+++ db.php
@@ -1,6 +1,7 @@
 <?php
 
-function mysql_escape_utf8($string) {
+function mysql_escape_utf8($string)
+{
     // Ensure the string is in UTF-8 encoding
     $string = mb_convert_encoding($string, 'UTF-8', 'UTF-8');
 
@@ -11,13 +12,13 @@ function mysql_escape_utf8($string) {
     return str_replace($search, $replace, $string);
 }
 
-function chat_query($sql) {
-    $link = mysqli_connect("127.0.0.1","root","","chat");
+function chat_query($sql)
+{
+    $link = mysqli_connect("127.0.0.1", "root", "", "chat");
     if (!$link) {
-        exit("Connection unsuccessful") .mysqli_error($link);
+        exit("Connection unsuccessful") . mysqli_error($link);
     }
     $result = mysqli_query($link, $sql);
     mysqli_close($link);
     return $result;
-
 }
