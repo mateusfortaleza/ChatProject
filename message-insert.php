3158f0c3ce9b4053fb254228317fd6658ed7fc5e
Mateus Fortaleza <mateusfortaleza@protonmail.com>
2025-03-14 10:14:20 -0300

    Minor modifications

--- message-insert.php
+++ message-insert.php
@@ -2,13 +2,12 @@
 require "db.php";
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     date_default_timezone_set("AMERICA/SAO_PAULO");
-    $response = ['success' => false, 'message' => ''];
     $text = $_POST['chatText'];
     $user = $_POST['chatUser'];
     $date = date("Y-m-d H:i:s");
 
     $sql = "INSERT INTO messages (Sender, Message, CreatedAt) VALUES ('$user', '$text', '$date')";
     chat_query($sql);
-    echo "{ \"insertMessage\": \"success\" }";
     header("Content-Type: application/json");
+    echo json_encode(["insertMessage" => "success"]);
 }
